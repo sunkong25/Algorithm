7d2fa7a03cd3f9ff83b678edc32480e608772cf3
sunkong25 <kcindy325k@kyonggi.ac.kr>
2024-12-09 10:15:19 +0900

    [FEAT] 05-1 깊이 우선 탐색 내용 정리

---
+++ src/Doit_알고리즘_코딩테스트/정렬/퀵정렬/P11004_K번째수구하기.java
@@ -0,0 +1,38 @@
+package Doit_알고리즘_코딩테스트.정렬.퀵정렬;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.security.Key;
+import java.util.StringTokenizer;
+
+public class P11004_K번째수구하기 {
+  /*  public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+    }
+
+    public static int quickSort(int start, int end, int K) {
+        int p = pivot(start, end);
+        if (p == K) {
+            return K;
+        } else if (K < p) {
+            return quickSort(start, p - 1, K);
+        } else {
+            return quickSort(p + 1, end, K);
+        }
+    }
+
+    public static int pivot(int start, int end) {
+
+    }*/
+}
