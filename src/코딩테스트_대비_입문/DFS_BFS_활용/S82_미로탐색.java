077c996183cbe57edb50952fea44d7ccadedaed5
sunkon25 <kcindy325k@kyonggi.ac.kr>
2025-12-23 17:07:59 +0900

    Feat: 82. 미로탐색 추가
    - P82_미로탐색
    - S82_미로탐색

--- src/코딩테스트_대비_입문/DFS_BFS_활용/S82_미로탐색.java
+++ src/코딩테스트_대비_입문/DFS_BFS_활용/S82_미로탐색.java
@@ -2,10 +2,10 @@ package 코딩테스트_대비_입문.DFS_BFS_활용;
 
 import java.util.Scanner;
 
-public class P82_미로탐색 {
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
-	static int[][] v = new int[8][8], m = new int[8][8];
+public class S82_미로탐색 {
+	static int[] dx = {-1, 0, 1, 0};
+	static int[] dy = {0, 1, 0, -1};
+	static int[][] board = new int[8][8];
 	static int answer = 0;
 
 	public void DFS(int x, int y) {
@@ -13,27 +13,29 @@ public class P82_미로탐색 {
 			answer++;
 		} else {
 			for (int i = 0; i < 4; i++) {
-				if (x > 0 && y > 0 && x <= 7 && y <= 7 && v[x][y] == 0 && m[x][y] == 0) {
-					v[x][y] = 1;
-					DFS(x + dx[i], y + dy[i]);
-					v[x][y] = 0;
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (nx >= 1 && nx <= 7 && ny >= 1 && ny <= 7 && board[nx][ny] == 0) {
+					board[nx][ny] = 1;
+					DFS(nx, ny);
+					board[nx][ny] = 0;
 				}
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-		P82_미로탐색 T = new P82_미로탐색();
+		S82_미로탐색 T = new S82_미로탐색();
 		Scanner sc = new Scanner(System.in);
 
 		for (int i = 1; i <= 7; i++) {
 			for (int j = 1; j <= 7; j++) {
-				m[i][j] = sc.nextInt();
+				board[i][j] = sc.nextInt();
 			}
 		}
 
+		board[1][1] = 1;
 		T.DFS(1, 1);
 		System.out.println(answer);
 	}
 }
-
