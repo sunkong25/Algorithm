26ead9e030686107c80bb5294e6a98e8f6b1a8df
sunkon25 <kcindy325k@kyonggi.ac.kr>
2026-01-05 11:51:49 +0900

    Feat: 03. 잃어버린 강아지 추가
    - P03_잃어버린강아지
    - S03_잃어버린강아지

---
+++ src/자바_코딩테스트_it_대기업_유제/section02_시뮬레이션_구현/S03_잃어버린강아지.java
@@ -0,0 +1,85 @@
+package 자바_코딩테스트_it_대기업_유제.section02_시뮬레이션_구현;
+
+public class S03_잃어버린강아지 {
+	public int solution(int[][] board) {
+		int answer = 0;
+		int n = board.length;
+		int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+		int[] dx = {-1, 0, 1, 0};
+		int[] dy = {0, 1, 0, -1};
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (board[i][j] == 2) {
+					x1 = i;
+					y1 = j;
+				} else if (board[i][j] == 3) {
+					x2 = i;
+					y2 = j;
+				}
+			}
+		}
+
+		int d1 = 0, d2 = 0;
+
+		while (answer < 10000) {
+			answer++;
+			boolean flag1 = true, flag2 = true;
+			int nx1 = x1 + dx[d1];
+			int ny1 = y1 + dy[d1];
+			int nx2 = x2 + dx[d2];
+			int ny2 = y2 + dy[d2];
+
+			if (nx1 < 0 || nx1 >= n || ny1 < 0 || ny1 >= n || board[nx1][ny1] == 1) {
+				d1 = (d1 + 1) % 4;
+				flag1 = false;
+			}
+			if (nx2 < 0 || nx2 >= n || ny2 < 0 || ny2 >= n || board[nx2][ny2] == 1) {
+				d2 = (d2 + 1) % 4;
+				flag2 = false;
+			}
+
+			if (flag1) {
+				x1 = nx1;
+				y1 = ny1;
+			}
+			if (flag2) {
+				x2 = nx2;
+				y2 = ny2;
+			}
+			if (x1 == x2 && y1 == y2) {
+				break;
+			}
+		}
+		if (answer >= 10000) {
+			return 0;
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		S03_잃어버린강아지 T = new S03_잃어버린강아지();
+		int[][] arr1 = {{0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+				{0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+				{0, 0, 0, 1, 0, 0, 0, 1, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 1, 0, 0, 0, 2, 0, 0},
+				{1, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+				{0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 3, 0, 0, 0, 1},
+				{0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+				{0, 1, 0, 1, 0, 0, 0, 0, 0, 0}};
+		System.out.println(T.solution(arr1));
+		int[][] arr2 = {{1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+				{0, 0, 1, 1, 0, 0, 0, 1, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+				{0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+				{0, 0, 1, 0, 0, 0, 0, 0, 2, 1},
+				{0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
+				{0, 1, 0, 1, 0, 0, 0, 0, 0, 3}};
+		System.out.println(T.solution(arr2));
+	}
+}
