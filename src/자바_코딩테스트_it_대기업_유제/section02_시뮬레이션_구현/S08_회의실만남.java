93d63f197c9e0eb92b43daea0c3cb9d2ff1aa34b
sunkon25 <kcindy325k@kyonggi.ac.kr>
2026-01-08 15:36:43 +0900

    feat: 08. 회의실 만남 추가
    - S08_회의실만남

---
+++ src/자바_코딩테스트_it_대기업_유제/section02_시뮬레이션_구현/S08_회의실만남.java
@@ -0,0 +1,47 @@
+package 자바_코딩테스트_it_대기업_유제.section02_시뮬레이션_구현;
+
+import java.util.Arrays;
+
+public class S08_회의실만남 {
+	public int[] solution(int[] enter, int[] exit) {
+		int[] answer = {};
+		int n = enter.length;
+		int[] enterIdx = new int[n], enterT = new int[n], exitT = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			enter[i]--;
+			exit[i]--;
+		}
+		for (int i = 0; i < n; i++) {
+			enterIdx[enter[i]] = i;
+		}
+		int cnt = 0;
+		for (int i = 0, j = 0; i < n; i++) {
+			while (j < n && j <= enterIdx[exit[i]]) {
+				enterT[enter[j]] = cnt++;
+				j++;
+			}
+			exitT[exit[i]] = cnt++;
+		}
+		answer = new int[n];
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (!(exitT[i] < enterT[j] || exitT[j] < enterT[i])) {
+					answer[i]++;
+					answer[j]++;
+				}
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		S08_회의실만남 T = new S08_회의실만남();
+		System.out.println(Arrays.toString(T.solution(new int[]{1, 2, 3, 4}, new int[]{2, 4, 1, 3})));
+		System.out.println(Arrays.toString(T.solution(new int[]{1, 2, 5, 3, 4}, new int[]{2, 3, 1, 4, 5})));
+		System.out.println(
+				Arrays.toString(T.solution(new int[]{1, 3, 2, 4, 5, 7, 6, 8}, new int[]{2, 3, 5, 6, 1, 4, 8, 7})));
+		System.out.println(Arrays.toString(T.solution(new int[]{1, 4, 7, 2, 3, 5, 6}, new int[]{5, 2, 6, 1, 7, 3, 4})));
+		System.out.println(Arrays.toString(T.solution(new int[]{1, 4, 2, 3}, new int[]{2, 1, 4, 3})));
+	}
+}
