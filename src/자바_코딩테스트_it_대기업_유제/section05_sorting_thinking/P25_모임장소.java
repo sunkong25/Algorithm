c5919fede31436e472ef6c6ed632ea77b3963e1f
sunkon25 <kcindy325k@kyonggi.ac.kr>
2026-02-04 12:30:53 +0900

    feat: 25. 모임 장소 추가
    - P25_모임장소
    - S25_모임장소

---
+++ src/자바_코딩테스트_it_대기업_유제/section05_sorting_thinking/P25_모임장소.java
@@ -0,0 +1,43 @@
+package 자바_코딩테스트_it_대기업_유제.section05_sorting_thinking;
+
+import java.util.ArrayList;
+
+public class P25_모임장소 {
+	public int solution(int[][] board) {
+		int answer = Integer.MAX_VALUE;
+		int n = board.length;
+
+		ArrayList<Integer[]> a = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (board[i][j] == 1) {
+					a.add(new Integer[] {i, j});
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				int sum = 0;
+				for (int k = 0; k < a.size(); k++) {
+					sum += Math.abs(i - a.get(k)[0]) + Math.abs(j - a.get(k)[1]);
+				}
+				answer = Math.min(answer, sum);
+			}
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		P25_모임장소 T = new P25_모임장소();
+		System.out.println(T.solution(
+				new int[][]{{1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}}));
+		System.out.println(T.solution(
+				new int[][]{{1, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}}));
+		System.out.println(T.solution(
+				new int[][]{{1, 0, 0, 0, 1, 1}, {0, 1, 0, 0, 1, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0},
+						{0, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 1, 1}}));
+	}
+}
